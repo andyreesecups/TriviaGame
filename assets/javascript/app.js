295c11fe9a127487bbea453d22ef34a6dd51c0e2
andyreesecups <andyreesecups@yahoo.com>
2017-05-06 16:14:20 -0500

    functionality working, completely redid javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,135 +1,188 @@
-var timeRemaining;
-var score;
-var answer;
-var question;
-var counter;
+var panel = $("#quiz-area");
+var countStartNumber = 30;
 
-var questArr = [
-    new Questions("Which Spice Girl is the only one actually named after a spice?", ["Ginger Spice", "Pepper Spice", "Paprica Spice", "Rosemary Spice"]),
-    new Questions("What continent has the fewest flowering plants?", ["Antarctica", "Africa", "Australia", "South America"]),
-    new Questions("How many NBA championships have the San Antonio Spurs won?", ["Five", "Three", "Seven", "Four"]),
-    new Questions("Who averaged one patent for every three weeks of his life?", ["Thomas Edison", "Albert Einstein", "Bill Nye", "Benjamin Franklin"]),
-    new Questions("What is the diameter of the Earth?", ["8,000 miles", "2,000 miles", "5,000 miles", "10,000 miles"]),
-    new Questions("What is the only land mammal that can't jump?", ["Elephant", "Giraffe", "Cow", "Turtle"]),
-    new Questions("Which one of these celebrities is NOT a convicted felon?", ["Snoop Dogg", "Martha Stewart", "Robert Downey Jr", "Tim Allen"]),
-    new Questions("What is the fastest fish in the ocean? ", ["Sailfish", " Tuna", "Blue Marlin", ""]),
-    new Questions("What year did the New Orleans Saints win the Super Bowl", ["2010", "2006", "2012", "2003"])
-];
+// Question set
+var questions = [{
+  question: "How many championships has the San Antonio Spurs won?",
+  answers: ["0", "3", "5", "6"],
+  correctAnswer: "5",
+  image: "assets/images/spurs.gif"
+}, {
+  question: "Which rapper made headlines with his silly 'Hotline Bling' dance?",
+  answers: ["Snoop Dogg", "Drake", "Fred Durst", "Eminem"],
+  correctAnswer: "Drake",
+  image: "assets/images/drake.gif"
+}, {
+  question: "What year was the 'best national championship win' ever? Hint: Vince Young",
+  answers: ["1994", "1988", "2007", "2006"],
+  correctAnswer: "2006",
+  image: "assets/images/ut.gif"
+}, {
+  question: "What day of the year has been adopted as 'Star Wars Day'?",
+  answers: ["May 4th", "April 18th", "December 25th", "July 4th"],
+  correctAnswer: "May 4th",
+  image: "assets/images/starwars.gif"
+}, {
+  question: "What is the best show in the world?",
+  answers: ["Game of Thrones", "Seinfeld", "Power Rangers", "Dragonball Z"],
+  correctAnswer: "Game of Thrones",
+  image: "assets/images/tyrion.gif"
+}, {
+  question: "What is the fastest fish in the ocean?",
+  answers: ["Tiger Shark", "Sailfish", "Tuna", "Blue Marlin"],
+  correctAnswer: "Sailfish",
+  image: "assets/images/sailfish.gif"
+}, {
+  question: "In which city where the 2016 summer Olympics held?",
+  answers: ["Rio, Brazil", "Jackson, Mississippi", "Flint, Michigan", "Anchorage, Alaska"],
+  correctAnswer: "Rio, Brazil",
+  image: "assets/images/phelps.gif"
+}, {
+  question: "Who won the 2016 Best Actor Oscar?",
+  answers: ["Seth Rogen", "Matthew Mcconaughey", "Leonardo Dicaprio", "Casey Affleck"],
+  correctAnswer: "Leonard Dicaprio",
+  image: "assets/images/leo.gif"
+}];
 
+// Variable to hold our setInterval
+var timer;
 
+var game = {
 
-var usedQuestions = [];
+  questions: questions,
+  currentQuestion: 0,
+  counter: countStartNumber,
+  correct: 0,
+  incorrect: 0,
 
-function Questions(question, answers, img, time) {
-    this.question = question;
-    this.answers = answers;
-    this.img = img || null;
-    this.time = time || 9; //seconds
-}
-
-
-
-// Display a random question object's question, answers, and handled time remaining
-//------------------------------------------------------------------
-function displayRandom() {
- 
- 	$("#question").empty();
-
-
-    // pick a random question object from the questArr
-    //-----------------------------------------------
-
-    // picking a random index between 0 and one less than the length of the questArr
-    var index = Math.floor(Math.random() * questArr.length);
-
-    // picking the random question objects from the questArr array
-    var questObj = questArr[index];
-
-    // Display the random question object's question
-    //-----------------------------------------------
-
-    // pull question from the random question object
-    var question = questObj.question;
-
-    // display question
-    $("#question").append("<p> " + question + "</p>");
-
-    
-
-
-    // Display the random question object's answers
-    //-----------------------------------------------
-
-    // pull the answers array from the random question object
-    var answers = questObj.answers;
-
-
-    // pick the answers from the answers array
-    //-----------------------------------------------
-    // loop through the array 
-    for (var i = 0; i < answers.length; i++) {
-        //pick the answer from the answers array at the index 'i'
-        var answer = answers[i];
-
-        //display answer in console
-        // console.log(answer);
-        $("#question").append("<p> " + answer + "</p>");
-        
-        
-
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.timeUp();
     }
+  },
 
+  loadQuestion: function() {
 
+    timer = setInterval(game.countdown, 1000);
 
+    panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
 
-    // Display the random question object's time remaining
-    //-----------------------------------------------
+    for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
+      panel.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
+      + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
+    }
+  },
 
-    //pulled the time from the random question object
-    var counter = questObj.time; // var counter = 5;
-    
-        
-    // create an interval to calculate time remaining
-    var interval = setInterval(function() {
-        //decrement counter to calculate time remaining
-        counter--;
+  nextQuestion: function() {
+    game.counter = countStartNumber;
+    $("#counter-number").html(game.counter);
+    game.currentQuestion++;
+    game.loadQuestion();
+  },
 
-        // displaying time remaining in console	
-        console.log(counter);
+  timeUp: function() {
 
-        // Handle running out of time
-        if (counter <= 0) {
-            // stop calculating time remaining 
-            clearInterval(interval);
-            shiftQuestionObj(index);
-            
-        }
-    }, 1000);
+    clearInterval(timer);
 
-var display = ("<p> " + "Time Remaining:" + counter + "</p>");
-    $("#timeremaining").html(display);
+    $("#counter-number").html(game.counter);
 
-}
+    panel.html("<h2>Out of Time!</h2>");
+    panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
+    panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
 
-// Put the random question object in the usedQuestions array and remove it from the questArr array
-//--------------------------------------------------------------------------------------------------
-function shiftQuestionObj(index) {
-    // Remove the random question object from the questArr array
-    var object = questArr.splice(index, 1);
+    if (game.currentQuestion === questions.length - 1) {
+      setTimeout(game.results, 3 * 1000);
+    }
+    else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
 
-    // Push the random question object into the usedQuestions array
-    usedQuestions.push(object[0]);
+  results: function() {
 
-    console.log(questArr);
-    console.log(usedQuestions);
+    clearInterval(timer);
 
-}
+    panel.html("<h2>All done, heres how you did!</h2>");
 
+    $("#counter-number").html(game.counter);
 
-// Once the Start button is clicked generate a random Question and start counter
-document.getElementById("startreset").onclick = function() {
-    displayRandom();
+    panel.append("<h3>Correct Answers: " + game.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (game.incorrect + game.correct)) + "</h3>");
+    panel.append("<br><button id='start-over'>Start Over?</button>");
+  },
 
-}
+  clicked: function(e) {
+    clearInterval(timer);
+    if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+      this.answeredCorrectly();
+    }
+    else {
+      this.answeredIncorrectly();
+    }
+  },
+
+  answeredIncorrectly: function() {
+
+    game.incorrect++;
+
+    clearInterval(timer);
+
+    panel.html("<h2>Nope!</h2>");
+    panel.append("<h3>The Correct Answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
+    panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+
+    if (game.currentQuestion === questions.length - 1) {
+      setTimeout(game.results, 3 * 1000);
+    }
+    else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+
+  answeredCorrectly: function() {
+
+    clearInterval(timer);
+
+    game.correct++;
+
+    panel.html("<h2>Correct!</h2>");
+    panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+
+    if (game.currentQuestion === questions.length - 1) {
+      setTimeout(game.results, 3 * 1000);
+    }
+    else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+
+  reset: function() {
+    this.currentQuestion = 0;
+    this.counter = countStartNumber;
+    this.correct = 0;
+    this.incorrect = 0;
+    this.loadQuestion();
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start-over", function() {
+  game.reset();
+});
+
+$(document).on("click", ".answer-button", function(e) {
+  game.clicked(e);
+});
+
+$(document).on("click", "#start", function() {
+  $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+  game.loadQuestion();
+});
+
 
 
